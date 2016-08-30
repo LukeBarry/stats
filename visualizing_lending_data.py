f626d6d39a8eca87df64a94e857895bdee822034
Luke Barry <luke-barry@hotmail.com>
2016-08-30 16:16:45 +0000

    Initial commit

--- visualizing_lending_data.py
+++ visualizing_lending_data.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
+import collections
 
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')  # load the data
 
@@ -12,8 +14,23 @@ loansData.hist(column='Amount.Funded.By.Investors')  # histogram
 plt.show()
 
 # generate a qq plot
-import scipy.stats as stats
-
 plt.figure()
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
 plt.show()
+
+# apply chi squared test to lending data
+from scipy import stats
+# Load the reduced version of the Lending Club Dataset
+loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+# Apply the collections.Counter() method on the number of open credit lines in the Loans data (Open.CREDIT.Lines)
+# to get counts of observations for each number of credit lines.
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+plt.figure()
+plt.bar(freq.keys(), freq.values(), width=1)
+plt.show()
+
+
+
