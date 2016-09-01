d09dd76d25850830fcc248feabc9394c7b4fe47e
Luke Barry <luke-barry@hotmail.com>
2016-09-01 17:47:36 +0000

    Innitial Commit

---
+++ logistic.py
@@ -0,0 +1,36 @@
+import numpy as np
+import statsmodels.api as sm
+import pandas as pd
+import math
+import pylab
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
+loansData['Interest.Rate'] = cleanInterestRate
+cleanLoanLength = loansData['Loan.Length'].map(lambda x: int(x.rstrip(' months')))
+loansData['Loan.Length'] = cleanLoanLength
+loansData['FICO.Score'] = [int(val.split('-')[0]) for val in loansData['FICO.Range']]
+loansData['constant'] = 1
+IR_TF = loansData['Interest.Rate'].map(lambda x: int(x >= .12))
+loansData['IR_TF'] = IR_TF
+loansData.to_csv('loansData_clean.csv', header=True, index=False)
+ind_vars = ['Loan.Length', 'FICO.Score', 'constant']
+
+df = pd.read_csv("loansData_clean.csv")
+
+logit = sm.Logit(df['IR_TF'], df[ind_vars])
+result = logit.fit()
+
+coeff = result.params
+print coeff
+
+FicoScore = 720
+LoanAmount = 10000
+logistic_function = 1/(1 + math.exp(coeff[2] + coeff[1]*(FicoScore) - coeff[0]*(LoanAmount)))
+
+print logistic_function
+
+x = np.linspace(0,950,1000)
+y = 1/(1 + np.exp(coeff[2] + coeff[1]*(x) - coeff[0]*(10000)))
+pylab.plot(x,y)
+pylab.show()
