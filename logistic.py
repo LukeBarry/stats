577dd63ef2ed83a2638f252527655a38ccb128d4
Luke Barry <luke-barry@hotmail.com>
2016-09-07 14:27:31 +0000

    updated file

--- logistic.py
+++ logistic.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-import math
+from math import e
 import pylab
 import matplotlib.pyplot as plt
-
+''' Cleaning the data
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
 loansData['Interest.Rate'] = cleanInterestRate
@@ -15,28 +15,31 @@ loansData['constant'] = 1
 IR_TF = loansData['Interest.Rate'].map(lambda x: int(x >= .12))
 loansData['IR_TF'] = IR_TF
 loansData.to_csv('loansData_clean.csv', header=True, index=False)
-ind_vars = ['Loan.Length', 'FICO.Score', 'constant']
+'''
 
 # What is the probability of getting a loan from the Lending Club for $10,000
 # at an interest rate less than or equal to 12% with a FICO score of 750?
 
-df = pd.read_csv("loansData_clean.csv")
+df = pd.read_csv("loansData_clean.csv")  # cleaning data wasn't needed, loading clean data here
+ind_vars = ['Amount.Funded.By.Investors', 'FICO.Score', 'constant']
 logit = sm.Logit(df['IR_TF'], df[ind_vars])
 result = logit.fit()
 
 coeff = result.params
 print coeff
 
-FicoScore = 750
+FicoScore = 720
 LoanAmount = 10000
 
-logistic_function = 1/(1 + math.exp(coeff[2] + coeff[1]*(FicoScore) - coeff[0]*(LoanAmount)))
-print logistic_function
+def logistic_function(LoanAmount, FicoScore, coeff):
+    px = 1 / (1 + e **(coeff[2] + coeff[1]*(FicoScore) + coeff[0]*(LoanAmount)))
+    print (coeff[2] + coeff[1]*(FicoScore) + coeff[0]*(LoanAmount))
+    return px
 
-x = np.linspace(0,950,1000)
-y = 1/(1 + np.exp(coeff[2] + coeff[1]*(x) - coeff[0]*(10000)))
-pylab.plot(x,y)
-pylab.show()
+print(logistic_function(LoanAmount, FicoScore, coeff))
+
+
+
 
 
 
